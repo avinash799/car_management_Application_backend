b879596aaf8d39cbcd38f49d0e412e6c2b27b490
avinash <avinashrai16042002@gmail.com>
2024-11-18 11:10:11 +0530

    minor error fix

--- config/index.js
+++ config/index.js
@@ -1,29 +1,8 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI);
-//         console.log('MongoDB connected');
-//     } catch (err) {
-//         console.error('Error connecting to MongoDB', err);
-//         process.exit(1);
-//     }
-// };
-// export default connectDB;
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
 
-dotenv.config();  // Ensure environment variables are loaded
-
-const connectDB = async () => {
+export const connectDB = async () => {
     try {
-        const uri = process.env.MONGO_URI;
-        if (!uri) {
-            console.error('MongoDB URI is undefined');
-            process.exit(1);
-        }
-
-        await mongoose.connect(uri);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
     } catch (err) {
         console.error('Error connecting to MongoDB', err);
@@ -31,4 +10,29 @@ const connectDB = async () => {
     }
 };
 
-export default connectDB;
+
+
+
+// export default connectDB;
+// import mongoose from "mongoose";
+// import dotenv from 'dotenv';
+
+// dotenv.config();  // Ensure environment variables are loaded
+
+// const connectDB = async () => {
+//     try {
+//         const uri = process.env.MONGO_URI;
+//         if (!uri) {
+//             console.error('MongoDB URI is undefined');
+//             process.exit(1);
+//         }
+
+//         await mongoose.connect(uri);
+//         console.log('MongoDB connected');
+//     } catch (err) {
+//         console.error('Error connecting to MongoDB', err);
+//         process.exit(1);
+//     }
+// };
+
+// export default connectDB;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from './config/index.js';
+import { connectDB } from './config/index.js';
 import authRoutes from './routes/auth.js';
 import carRoutes from './routes/car.js';
 
