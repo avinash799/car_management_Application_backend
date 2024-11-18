d32c241dad1ec5e9bb3cbd9e26e1d6af70b12e43
avinash <avinashrai16042002@gmail.com>
2024-11-18 11:19:38 +0530

    minor changes

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error('Error connecting to MongoDB', err);
-        process.exit(1);
-    }
-};
+// export const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI);
+//         console.log('MongoDB connected');
+//     } catch (err) {
+//         console.error('Error connecting to MongoDB', err);
+//         process.exit(1);
+//     }
+// };
 
 
 
@@ -36,3 +36,31 @@ export const connectDB = async () => {
 // };
 
 // export default connectDB;
+
+import dotenv from 'dotenv';
+
+// Load environment variables from .env
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const uri = process.env.MONGO_URI;  // Get MONGO_URI from environment variables
+
+        if (!uri) {
+            console.error('MongoDB URI is undefined');
+            process.exit(1);  // Exit the process if MONGO_URI is undefined
+        }
+
+        console.log('MongoDB URI:', uri);  // Log the URI for debugging
+
+        // Connect to MongoDB without deprecated options
+        await mongoose.connect(uri);
+
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('Error connecting to MongoDB', err);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
